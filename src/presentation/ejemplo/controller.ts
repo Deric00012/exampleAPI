7fb14f736d48cc18239a0220066d998358607666
Deric David Hernandez Cardona <Deric.Hernandez@safary.local>
2025-01-23 12:30:00 -0600

    Segundo commit

--- src/presentation/ejemplo/controller.ts
+++ src/presentation/ejemplo/controller.ts
@@ -4,20 +4,9 @@ import { prisma } from "../../data/postgres";
 export class EjemploApiController {
     public async getAll(req: Request, res: Response): Promise<void> {
         const example = await prisma.$queryRaw<any>`
-        select 
-        sl.inicio - interval '6 hours' as inicio,
-        sl.fin  - interval '6 hours' as fin,
-        fe.alternative_code as alternative_code ,
-        fe.names as nombres,
-        fe.last_names as apellidos,
-        sl.nombre ,
-        sl.version_app,
-        sl.id_device 
-        from sync_log sl 
-        join fcpy_employees fe on fe.id = sl.codigo_empleado
-        where sl.nombre = 'agritask MO'
-        order by sl.id desc
-        limit 300;
+        select *
+        from sec_users
+        limit 10;
         `;
         res.send(example);
     }
